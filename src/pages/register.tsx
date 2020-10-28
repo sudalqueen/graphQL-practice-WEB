55be28cfa7982cbb34b3b4c6d7fb6f2c672271fb
sudalqueen <abbey17@cnu.ac.kr>
2020-10-28 16:42:33 +0900

    add: GraphQ: Code Generator

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,25 +4,12 @@ import { Box, Button } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
-const REGISTER_INPUT = `
-mutation Register($username: String!, $password: String!) {
-  register(options: {username: $username, password: $password}) {
-    errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }
-}`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_INPUT);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
