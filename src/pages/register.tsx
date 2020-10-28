99f87288b7b6080fdfced60b6e2b6658e26491f1
sudalqueen <abbey17@cnu.ac.kr>
2020-10-28 15:25:26 +0900

    add: Next.js + Chakra

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React from "react";
+import { Form, Formik } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Box,
+  Button,
+} from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 
-interface registerProps {
-    
-}
+interface registerProps {}
 
-export const register: React.FC<registerProps> = ({}) => {
-    return ();
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="Password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
+
+export default Register;
